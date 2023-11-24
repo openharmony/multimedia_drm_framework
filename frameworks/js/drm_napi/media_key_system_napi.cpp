439f4d20c7d0725a2bb3161002112079cf810fe0
gezhe <gezhe1@huawei.com>
2023-11-24 14:19:28 +0800

    修改问题单
    Signed-off-by: gezhe <gezhe1@huawei.com>

--- frameworks/js/drm_napi/media_key_system_napi.cpp
+++ frameworks/js/drm_napi/media_key_system_napi.cpp
@@ -246,9 +246,13 @@ napi_value MediaKeySystemNapi::CreateMediaKeySession(napi_env env, napi_callback
     if (argc == ARGS_ZERO) {
         jsSecurityLevel = IMediaKeySessionService::SECURITY_LEVEL_UNKNOWN;
     } else {
-        status = napi_get_value_int32(env, argv[PARAM0], &jsSecurityLevel);
-        if (status != napi_ok) {
-            DRM_ERR_LOG("MediaKeySystemNapi napi get jsSecurityLevel failure!");
+        DRM_CHECK_AND_RETURN_RET_LOG(napi_get_value_int32(env, argv[PARAM0], &jsSecurityLevel) ==
+            napi_ok, nullptr, "MediaKeySystemNapi napi get jsSecurityLevel failure!");
+        IMediaKeySessionService::SecurityLevel securityLevel;
+        securityLevel = (IMediaKeySessionService::SecurityLevel)jsSecurityLevel;
+        if ((securityLevel < IMediaKeySessionService::SECURITY_LEVEL_UNKNOWN) ||
+            (securityLevel > IMediaKeySessionService::SECURITY_LEVEL_MAX)) {
+            DRM_ERR_LOG("SecurityLevel is invalid");
             return nullptr;
         }
     }
