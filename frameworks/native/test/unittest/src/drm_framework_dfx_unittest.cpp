92d25ce90b55ddea3f71492cdaf90dab17f88379
baoxy92 <baoxianyue@hisilicon.com>
2024-10-19 02:18:10 +0000

    update frameworks/native/test/unittest/src/drm_framework_dfx_unittest.cpp.
    Signed-off-by: baoxy92 <baoxianyue@hisilicon.com>

--- frameworks/native/test/unittest/src/drm_framework_dfx_unittest.cpp
+++ frameworks/native/test/unittest/src/drm_framework_dfx_unittest.cpp
@@ -112,7 +112,8 @@ HWTEST_F(DrmFrameworkUnitTest, Drm_unittest_ReportFaultEvent, TestSize.Level0)
     uint32_t errorCode = 0;
     std::string errorMesg = "GenerateMediaKeyRequest failed";
     std::string extraMesg = "";
-    ReportFaultEvent(errorCode, errorMesg, extraMesg);
+    Drm_ErrCode errNo = (Drm_ErrCode)ReportFaultEvent(errorCode, errorMesg, extraMesg);
+    EXPECT_EQ(errNo, 0);
 }
 
 HWTEST_F(DrmFrameworkUnitTest, Drm_unittest_ReportDecryptionFaultEvent, TestSize.Level0)
@@ -122,14 +123,17 @@ HWTEST_F(DrmFrameworkUnitTest, Drm_unittest_ReportDecryptionFaultEvent, TestSize
     std::string decryptAlgo = "decryptAlgo";
     std::string decryptKeyid = "decryptKeyid";
     std::string decryptIv = "decryptIv";
-    ReportDecryptionFaultEvent(errorCode, errorMesg, decryptAlgo, decryptKeyid, decryptIv);
+    Drm_ErrCode errNo = (Drm_ErrCode)ReportDecryptionFaultEvent(errorCode, errorMesg, decryptAlgo, decryptKeyid,
+        decryptIv);
+    EXPECT_EQ(errNo, 0);
 }
 
 HWTEST_F(DrmFrameworkUnitTest, Drm_unittest_ReportServiceBehaviorEvent, TestSize.Level0)
 {
     std::string serviceName = "DRM_OEM_SERVICE";
     std::string action = "start";
-    ReportServiceBehaviorEvent(serviceName, action);
+    Drm_ErrCode errNo = (Drm_ErrCode)ReportServiceBehaviorEvent(serviceName, action);
+    EXPECT_EQ(errNo, 0);
 }
 
 HWTEST_F(DrmFrameworkUnitTest, Drm_unittest_ReportLicenseBehaviorEvent, TestSize.Level0)
@@ -143,7 +147,8 @@ HWTEST_F(DrmFrameworkUnitTest, Drm_unittest_ReportLicenseBehaviorEvent, TestSize
         "clearplay_v1",
         "bundleName",
     };
-    ReportLicenseBehaviorEvent(statisticsInfo, mediaKeyType, downLoadInfo);
+    Drm_ErrCode errNo = (Drm_ErrCode)ReportLicenseBehaviorEvent(statisticsInfo, mediaKeyType, downLoadInfo);
+    EXPECT_EQ(errNo, 0);
 }
 
 HWTEST_F(DrmFrameworkUnitTest, Drm_unittest_ReportCertificateBehaviorEvent, TestSize.Level0)
@@ -160,8 +165,10 @@ HWTEST_F(DrmFrameworkUnitTest, Drm_unittest_ReportCertificateBehaviorEvent, Test
         "clearplay_v1",
         "bundleName",
     };
-    ReportCertificateBehaviorEvent(statisticsInfo, downLoadInfo, callServerTime, serverCostDuration,
+    Drm_ErrCode errNo = (Drm_ErrCode)ReportCertificateBehaviorEvent(statisticsInfo, downLoadInfo, callServerTime,
+        serverCostDuration,
         serverResult);
+    EXPECT_EQ(errNo, 0);
 }
 
 } // DrmStandard
